3a4596aa8ecba4fd723f6f83e4127a434ea8d9a2
samuel martins <samuel.nascimento@sou.inteli.edu.br>
2025-04-04 10:41:38 -0300

    criação do yaml

--- MyBlazorApp/obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
+++ MyBlazorApp/obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efed7710bf99fd16698ceb72265317838e72577d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
